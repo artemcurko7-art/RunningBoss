e112b9ed60f7e895a4b44268bc860398547579c8
artemcurko7-art <artemcurko7@gmail.com>
2026-01-26 22:03:00 +0300

    Init

--- Assets/Source/Player/MVP/Health/IHealthPresenter.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IHealthPresenter 
-{
-    void TakeDamage(int damage);
-    void Replenish(int health);
-}
